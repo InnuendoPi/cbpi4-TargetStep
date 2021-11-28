29246224c4164c19dd14ac959b394cf7e906c376
InnuendoPi <k.ader@pc-team.de>
2021-11-28 20:49:22 +0100

    0.0.2
    Signed-off-by: InnuendoPi <k.ader@pc-team.de>

--- cbpi4-TargetStep/__init__.py
+++ cbpi4-TargetStep/__init__.py
@@ -7,17 +7,22 @@ from cbpi.api.base import CBPiBase
 from voluptuous.schema_builder import message
 from cbpi.api.dataclasses import NotificationAction, NotificationType
 
+
 @parameters([Property.Number(label="Targettemp", configurable=True, description="Target temperature for kettle"),
             Property.Kettle(label="Kettle")])
-
 class TargetStep(CBPiStep):
-    
-    async def NextStep(self, **kwargs):
-        await self.next()
 
     async def on_start(self):
-        self.kettle = self.get_kettle(self.props.get("Kettle", None))
-        self.kettle.target_temp = int(self.props.get("Targettemp", 0))
+        try:
+            self.kettle = self.get_kettle(self.props.get("Kettle", None))
+            self.kettle.target_temp = int(self.props.get("Targettemp", 0))
+            #logging.info("TargetTemp Name: {} id: {} TargetTemp: {} ".format(self.kettle.name, self.kettle.id, self.kettle.target_temp))
+            await self.set_target_temp(self.kettle.id, self.kettle.target_temp)
+        except Exception as e:
+            logging.error("Failed to set Target Temp {} {}".format(id, e))
+
+    async def NextStep(self, **kwargs):
+        await self.next()
 
     async def on_stop(self):
         self.summary = ""
@@ -25,12 +30,11 @@ class TargetStep(CBPiStep):
 
     async def run(self):
         self.AutoNext = True
-        await self.push_update()
         self.cbpi.notify('Target temperature changed to: ', self.kettle.target_temp, NotificationType.INFO)
         await self.next()
 
         return StepResult.DONE
-		
+
 
 def setup(cbpi):
     '''
@@ -38,6 +42,6 @@ def setup(cbpi):
     Here you need to register your plugins at the server
     :param cbpi: the cbpi core 
     :return: 
-    '''    
-    
+    '''
+
     cbpi.plugin.register("TargetStep", TargetStep)
